14b67eb9b6d98861d79c730c5bf8e0cc1bc174af
Jonathan JG <jonysthil@gmail.com>
2022-06-29 23:37:19 -0500

    movie library

--- src/components/pages/Exercise02/index.js
+++ src/components/pages/Exercise02/index.js
@@ -15,62 +15,141 @@
 import "./assets/styles.css";
 import { useEffect, useState } from "react";
 
-export default function Exercise02 () {
-  const [movies, setMovies] = useState([])
-  const [fetchCount, setFetchCount] = useState(0)
-  const [loading, setLoading] = useState(false)
+const Exercise02 = () => {
 
-  const handleMovieFetch = () => {
-    setLoading(true)
-    setFetchCount(fetchCount + 1)
-    console.log('Getting movies')
-    fetch('http://localhost:3001/movies?_limit=50')
-      .then(res => res.json())
-      .then(json => {
-        setMovies(json)
-        setLoading(false)
-      })
-      .catch(() => {
-        console.log('Run yarn movie-api for fake api')
-      })
-  }
+	const [movies, setMovies] = useState([])
+	const [genres, setGenres] = useState([])
+	const [fetchCount, setFetchCount] = useState(0)
+	const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    handleMovieFetch()
-  }, [handleMovieFetch])
+	const handleMovieFetch = () => {
+		setFetchCount(fetchCount + 1)
+		//console.log('Getting movies')
+		fetch('http://localhost:3001/movies?_limit=50')
+			.then(res => res.json())
+			.then(json => {
+				setMovies(json)
+				setFetchCount(0)
+				setLoading(false)
+			})
+		.catch(() => {
+			console.log('Run yarn movie-api for fake api')
+		})
+	}
 
-  return (
-    <section className="movie-library">
-      <h1 className="movie-library__title">
-        Movie Library
-      </h1>
-      <div className="movie-library__actions">
-        <select name="genre" placeholder="Search by genre...">
-          <option value="genre1">Genre 1</option>
-        </select>
-        <button>Order Descending</button>
-      </div>
-      {loading ? (
-        <div className="movie-library__loading">
-          <p>Loading...</p>
-          <p>Fetched {fetchCount} times</p>
-        </div>
-      ) : (
-        <ul className="movie-library__list">
-          {movies.map(movie => (
-            <li key={movie.id} className="movie-library__card">
-              <img src={movie.posterUrl} alt={movie.title} />
-              <ul>
-                <li>ID: {movie.id}</li>
-                <li>Title: {movie.title}</li>
-                <li>Year: {movie.year}</li>
-                <li>Runtime: {movie.runtime}</li>
-                <li>Genres: {movie.genres.join(', ')}</li>
-              </ul>
-            </li>
-          ))}
-        </ul>
-      )}
-    </section>
-  )
+
+	const handleGenresFetch = () => {
+		fetch('http://localhost:3001/genres')
+			.then(res => res.json())
+			.then(json => {
+				setGenres(json)
+			})
+		.catch(() => {
+			console.log('Run yarn movie-api for fake api')
+		})
+	}
+
+	const selectCategory = (e) => {
+		if(e.target.value === '') {
+			alert ("Please select a category");
+			return false;
+		}
+		fetch('http://localhost:3001/movies?_limit=50&genres='+e.target.value)
+			.then(res => res.json())
+			.then(json => {
+				setMovies(json)
+			})
+		.catch(() => {
+			console.log('Run yarn movie-api for fake api')
+		})
+	}
+	
+	const oderMovie = (e) => {
+		if(e === '') {
+			alert ("Please select a order");
+			return false;
+		}
+
+		if(e === "ASC") {
+			const listAsc = [...movies].sort((a, b) => (a.year > b.year ? 1 : a.year < b.year ? -1 : 0))
+			setMovies(listAsc)
+		} else {
+			const listAsc = [...movies].sort((b, a) => (a.year > b.year ? 1 : a.year < b.year ? -1 : 0))
+			setMovies(listAsc)
+		}
+	}
+
+
+	useEffect(() => {
+		handleMovieFetch()
+		handleGenresFetch()
+	}, [])
+
+	return (
+		<>
+				<div className="header-movie-library"></div>
+			<section className="movie-library">
+				<div className="movie-container">
+					<h1 className="movie-library__title">
+						Movie Library
+					</h1>
+					<div className="">
+						
+						<div class="row">
+							<div className="col-md-12">
+								
+							<div className="input-group mb-3">
+								<select className="form-control" name="genre" onChange={(e) => selectCategory(e)} required placeholder="Search by genre...">
+									<option value="">Select Category</option>
+									{genres.map((item) =>
+									(<option value={item} key={item}>{item}</option>)
+									)}
+								</select>
+								<button className="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Select Order</button>
+								<ul className="dropdown-menu dropdown-menu-end">
+									<li><div className="dropdown-item" onClick={() => oderMovie('ASC')}>Year Ascending</div></li>
+									<li><hr className="dropdown-divider" /></li>
+									<li><div className="dropdown-item" onClick={() => oderMovie('DESC')}>Year Descending</div></li>
+								</ul>
+							</div>
+
+
+
+							</div>
+						</div>
+					</div>
+					{loading ? (
+						<div className="movie-library__loading">
+						<p>Loading...</p>
+						<p>Fetched {fetchCount} times</p>
+						</div>
+					) : (
+						<div>
+							<br></br>
+							<div className="row">
+							{movies.map(movie => (
+								<div className="col-12 col-md-3 mb-2" key={movie.id}>
+									<div className="card">
+										<img src={movie.posterUrl} alt={movie.title} className="card-img-top card-img" />
+										<div className="card-body">
+											<ul>
+												<li><b>{movie.title}</b></li>
+												<li>Year: {movie.year}</li>
+												<li>Runtime: {movie.runtime}</li>
+												<li>Genres: {movie.genres.join(', ')}</li>
+											</ul>
+										</div>
+									</div>
+
+								</div>
+							))}
+							</div>
+						</div>
+					)}
+				</div>
+			</section>
+		</>
+	)
 }
+
+export default Exercise02
